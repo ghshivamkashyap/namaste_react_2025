b1297fa462d0862a451cc8c6a15bb9c44deec8f4
shivam digiprod <lootforecast@gmail.com>
2025-03-31 22:30:09 +0530

    jsx vs core react done

--- class1/index.js
+++ class1/index.js
@@ -10,7 +10,14 @@ const element = React.createElement(
   "Hello, from reactJS js shivam!"
 );
 
+const jsxElement = (
+  <h1 className="greeting-class" id="greeting">
+    Hello, from reactJS js shivam! <br /> This is a JSX element.
+  </h1>
+);
+
 console.log("Element:", element);
+console.log("JSX Element:", jsxElement);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+root.render([element, jsxElement]);
